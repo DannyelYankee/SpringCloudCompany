2e33a0651ef1decdcc9f699a4c19aa80f59896ac
DannyelYankee <73075185+DannyelYankee@users.noreply.github.com>
2024-05-23 20:47:30 +0200

    Configure Eureka server

--- registry-server/src/main/java/com/yangli/registry_server/RegistryServerApplication.java
+++ registry-server/src/main/java/com/yangli/registry_server/RegistryServerApplication.java
@@ -2,12 +2,14 @@ package com.yangli.registry_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistryServerApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(RegistryServerApplication.class, args);
+    }
 
 }
