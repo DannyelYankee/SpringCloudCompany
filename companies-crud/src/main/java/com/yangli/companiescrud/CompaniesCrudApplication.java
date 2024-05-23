5bdd6c672be5432d86d783583ec1dc762148b84e
DannyelYankee <73075185+DannyelYankee@users.noreply.github.com>
2024-05-23 20:57:16 +0200

    Registry companies-crud to eureka

--- companies-crud/src/main/java/com/yangli/companiescrud/CompaniesCrudApplication.java
+++ companies-crud/src/main/java/com/yangli/companiescrud/CompaniesCrudApplication.java
@@ -2,12 +2,14 @@ package com.yangli.companiescrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompaniesCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompaniesCrudApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(CompaniesCrudApplication.class, args);
+    }
 
 }
